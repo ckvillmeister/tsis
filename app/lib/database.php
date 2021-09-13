cfdec988dbe8f1a7b80f5ec76ca570d66e551641
ckvillmeister <ckvillamor08@gmail.com>
2021-09-13 14:57:25 +0100

    Fast-forward

--- app/lib/database.php
+++ app/lib/database.php
@@ -18,7 +18,7 @@ class database{
 			mysqli_select_db($this->conn, $this->database_name);
 			$this->conn = new mysqli($this->hostname, $this->username, $this->password, $this->database_name);
 			//$this->conn->set_charset("ISO-8859-1");
-			$this->conn->set_charset("UTF8");
+			$this->conn->set_charset("utf8_mb4");
 			return $this->conn;
 		}
 	}
